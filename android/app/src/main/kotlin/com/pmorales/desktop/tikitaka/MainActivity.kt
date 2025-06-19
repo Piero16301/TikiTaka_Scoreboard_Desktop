35ad0748ce9d4e71b769effc452d5cb003f7dfba
Piero Angelo Morales Alcalde <piero.morales@utec.edu.pe>
2025-06-19 16:46:31 -0500

    Se quita android, ios y web

--- android/app/src/main/kotlin/com/pmorales/desktop/tikitaka/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.pmorales.desktop.tikitaka
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main_staging.dart
+++
@@ -1,6 +0,0 @@
-import 'package:tikitaka_scoreboard_desktop/app/app.dart';
-import 'package:tikitaka_scoreboard_desktop/bootstrap.dart';
-
-void main() {
-  bootstrap(() => const App());
-}
